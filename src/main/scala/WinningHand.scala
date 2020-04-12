54be247265b92b341def1e7239b0e29cbfd64f4c
Davis Bray <pdavisbray@gmail.com>
2020-04-12 13:31:43 -0500

    test

--- src/main/scala/WinningHand.scala
+++ src/main/scala/WinningHand.scala
@@ -1,18 +1,12 @@
 import scala.annotation.tailrec
 
-sealed abstract class WinningHand(rank: Int) extends Ordering[WinningHand] {
-  override def (that: WinningHand): Int = this.rank - that.rank
-}
-
-case class HighCard(cards: List[Card]) extends WinningHand(1) with Ordering[HighCard] {
-  def compare(that: HighCard): Int = compareFirstDifferentCard(cards, that.cards)
-
+protected object WinningHand {
   @tailrec
-  private def compareFirstDifferentCard(c1: List[Card], c2:List[Card]): Int = {
+  def compareFirstDifferentCard(c1: List[Card], c2:List[Card]): Int = {
     (c1, c2) match {
       case (Nil, Nil) => 0
       case (h1 :: t1, h2 :: t2) =>
-        if (h1 == h2) {
+        if (h1.num == h2.num) {
           return h1.compare(h2)
         } else {
           compareFirstDifferentCard(t1, t2)
@@ -25,3 +19,32 @@ case class HighCard(cards: List[Card]) extends WinningHand(1) with Ordering[High
     }
   }
 }
+
+sealed abstract class WinningHand(rank: Int) extends Ordered[WinningHand] {
+  def compare(that: WinningHand): Int = this.rank - that.rank
+}
+
+case class HighCard(cards: List[Card]) extends WinningHand(0){
+  override def compare(that: WinningHand): Int = {
+    that match {
+      case HighCard(thatCards) => WinningHand.compareFirstDifferentCard(cards, thatCards)
+      case other => super.compare(other)
+    }
+  }
+}
+
+case class OnePair(pair: Card, cards: List[Card]) extends WinningHand(1) {
+  override def compare(that: WinningHand): Int = {
+    that match {
+      case OnePair(thatPair, thatCards) =>
+        if (pair.num == thatPair.num) {
+          WinningHand.compareFirstDifferentCard(cards, thatCards)
+        } else {
+          pair.compare(thatPair)
+        }
+      case other => super.compare(other)
+    }
+  }
+}
+
+case class TwoPair(pairs: List[Card], cards)
