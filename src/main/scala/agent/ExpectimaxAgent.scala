0638408dba01e864b01bf2d77b32a888508aa0b8
Chet-Fagerstrom <cfagerst@trinity.edu>
2020-05-03 17:55:05 -0500

    Epectimax

--- src/main/scala/agent/ExpectimaxAgent.scala
+++ src/main/scala/agent/ExpectimaxAgent.scala
@@ -1,8 +1,11 @@
 package agent
-import game.{Call, Card, Fold, Move, PlayerList, Round}
+import game.{Call, Card, Fold, Move, PlayerList, Round, WinningHand}
 
 class ExpectimaxAgent(val name: String) extends Agent {
   import ExpectimaxAgent._
+
+  private var moneyLeft = 10000
+  var hand = null.asInstanceOf[List[Card]]
 
   val maxMoveTime = 10000
 
@@ -16,33 +19,63 @@ class ExpectimaxAgent(val name: String) extends Agent {
     next
   }
 
+  def scoreHand(node: Node): Int = ???
+
   override def getMove(round: Round, minAmt: Int, minimumRaise: Int): Move = {
 
-    def buildTree(root: Node, depth: Int): Unit = {
-      val unknownCards = getUnknownCards(root.river)
-      val validMoves = List(Call(minAmt), Fold)
-      if(root.playerList.hasNext){
-        validMoves.map(m => new PlayerNode(root, List.empty, ))
-        buildTree()
-        val nextPlay = new PlayerNode()
+    def expectimax(node: Node): Double = {
+
+      if(node.playerList.currentTurn != this) {
+        node.children match {
+          case Nil => scoreHand(node)
+          case c => c.map(expectimax).sum.toDouble / c.length
+        }
       } else {
-        val drawCard = new
+        node.children.map(expectimax).max
       }
+    }
+
+    def buildTree(root: Node, depth: Int): Unit = {
+
+        val unknownCards = getUnknownCards(root.river)
+        val validMoves = List(Call(minAmt), Fold())
+        if (root.playerList.hasNext) {
+          val kids = validMoves.map(m => new PlayerNode(root, List.empty, nextPlayerList(root.playerList), m, root.river))
+          root.children = kids
+          kids.foreach(k => buildTree(k, depth))
+        } else {
+          if (depth > 0) {
+            val kids = unknownCards.map(c => new ChanceNode(root, List.empty, nextPlayerList(root.playerList), c :: root.river))
+            root.children = kids
+            kids.foreach(k => buildTree(k, depth - 1))
+          }
+          else {
+
+          }
+        }
     }
 
     val root = new PlayerNode(null,List.empty, round.players.clone, null, round.getRiver())
     buildTree(root, 3)
+
+    try {
+      root.children.maxBy(expectimax).asInstanceOf[PlayerNode].moveMade
+    } catch {
+      case e: ClassCastException => throw new Exception ("Was a chance node not a player node")
+    }
+
+
   }
 
-  override def allIn(): Boolean = ???
+  override def allIn(): Boolean = moneyLeft == 0
 
-  override def getMoney: Int = ???
+  override def getMoney: Int = moneyLeft
 
-  override def hand: List[Card] = ???
+  override def pay(amt: Int): Unit = moneyLeft += amt
 
-  override def pay(amt: Int): Unit = ???
-
-  override def dealHand(cards: List[Card]): Unit = ???
+  override def dealHand(cards: List[Card]): Unit = {
+    hand = cards
+  }
 }
 
 object ExpectimaxAgent{
@@ -52,7 +85,6 @@ object ExpectimaxAgent{
 
   private class ChanceNode(parent: Node,
                            children: List[Node],
-                           val odds: Double,
                            playerList: PlayerList,
                            river: List[Card]) extends Node(parent, children, playerList, river){
 
